705584a30c003a436d6b80dec505484a18e76cd2
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-25 23:21:51 -0800

    Add some error checking/catching

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const findGif = () => {
-  const img = document.querySelector('img');
   const thing = document.querySelector('#thing');
   const url = `https://api.giphy.com/v1/gifs/translate?api_key=F4IlcBiLLi7W7nePrVK8jqcCrNmnneWv&s=${thing.value}`;
+  fetchGif(url);
+};
+
+const fetchGif = (url) => {
+  const img = document.querySelector('img');
   fetch(url, { mode: 'cors' })
+    .then((res) => {
+      if (res.ok) return Promise.resolve(res);
+      else return Promise.reject(res);
+    })
     .then((res) => res.json())
-    .then((res) => (img.src = res.data.images.original.url));
+    .then((res) => {
+      img.removeAttribute('hidden');
+      img.src = res.data.images.original.url;
+    })
+    .catch((err) => {
+      if (err.status == 401) console.log('401 error.');
+    });
 };
 
 document.querySelector('#find').addEventListener('click', findGif);
